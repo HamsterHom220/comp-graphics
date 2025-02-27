b40cf7412591eca703fac5669327f9d6ea3a8512
HamsterHom220 <sonyashulyak05@gmail.com>
2025-02-27 21:40:30 +0300

    1.1-1.2

--- src/renderer/rasterizer/rasterizer.h
+++ src/renderer/rasterizer/rasterizer.h
@@ -1,116 +1,44 @@
 #pragma once
 
-#include "resource.h"
+#include "settings.h"
+#include "world/camera.h"
+#include "world/model.h"
 
-#include <functional>
-#include <iostream>
-#include <linalg.h>
-#include <limits>
-#include <memory>
-
-
-using namespace linalg::aliases;
-
-static constexpr float DEFAULT_DEPTH = std::numeric_limits<float>::max();
 
 namespace cg::renderer
 {
-	template<typename VB, typename RT>
-	class rasterizer
+	class renderer
 	{
 	public:
-		rasterizer(){};
-		~rasterizer(){};
-		void set_render_target(
-				std::shared_ptr<resource<RT>> in_render_target,
-				std::shared_ptr<resource<float>> in_depth_buffer = nullptr);
-		void clear_render_target(
-				const RT& in_clear_value, const float in_depth = DEFAULT_DEPTH);
+		void set_settings(std::shared_ptr<cg::settings> in_settings);
 
-		void set_vertex_buffer(std::shared_ptr<resource<VB>> in_vertex_buffer);
-		void set_index_buffer(std::shared_ptr<resource<unsigned int>> in_index_buffer);
+		unsigned get_height();
+		unsigned get_width();
 
-		void set_viewport(size_t in_width, size_t in_height);
+		virtual void init() = 0;
+		virtual void destroy() = 0;
 
-		void draw(size_t num_vertexes, size_t vertex_offset);
+		virtual void update() = 0;
+		virtual void render() = 0;
 
-		std::function<std::pair<float4, VB>(float4 vertex, VB vertex_data)> vertex_shader;
-		std::function<cg::color(const VB& vertex_data, const float z)> pixel_shader;
+		void move_forward(float delta = 0.01f);
+		void move_backward(float delta = 0.01f);
+		void move_left(float delta = 0.01f);
+		void move_right(float delta = 0.01f);
+		void move_yaw(float delta = 0.f);
+		void move_pitch(float delta = 0.f);
 
 	protected:
-		std::shared_ptr<cg::resource<VB>> vertex_buffer;
-		std::shared_ptr<cg::resource<unsigned int>> index_buffer;
-		std::shared_ptr<cg::resource<RT>> render_target;
-		std::shared_ptr<cg::resource<float>> depth_buffer;
+		std::shared_ptr<cg::settings> settings;
 
-		size_t width = 1920;
-		size_t height = 1080;
+		std::shared_ptr<cg::world::camera> camera;
+		std::shared_ptr<cg::world::model> model;
 
-		int edge_function(int2 a, int2 b, int2 c);
-		bool depth_test(float z, size_t x, size_t y);
+		std::chrono::time_point<std::chrono::high_resolution_clock> current_time =
+				std::chrono::high_resolution_clock::now();
+		float frame_duration = 0.f;
 	};
 
-	template<typename VB, typename RT>
-	inline void rasterizer<VB, RT>::set_render_target(
-			std::shared_ptr<resource<RT>> in_render_target,
-			std::shared_ptr<resource<float>> in_depth_buffer)
-	{
-		// TODO Lab: 1.02 Implement `set_render_target`, `set_viewport`, `clear_render_target` methods of `cg::renderer::rasterizer` class
-		// TODO Lab: 1.06 Adjust `set_render_target`, and `clear_render_target` methods of `cg::renderer::rasterizer` class to consume a depth buffer
-	}
 
-	template<typename VB, typename RT>
-	inline void rasterizer<VB, RT>::set_viewport(size_t in_width, size_t in_height)
-	{
-		// TODO Lab: 1.02 Implement `set_render_target`, `set_viewport`, `clear_render_target` methods of `cg::renderer::rasterizer` class
-	}
-
-	template<typename VB, typename RT>
-	inline void rasterizer<VB, RT>::clear_render_target(
-			const RT& in_clear_value, const float in_depth)
-	{
-		// TODO Lab: 1.02 Implement `set_render_target`, `set_viewport`, `clear_render_target` methods of `cg::renderer::rasterizer` class
-		// TODO Lab: 1.06 Adjust `set_render_target`, and `clear_render_target` methods of `cg::renderer::rasterizer` class to consume a depth buffer
-	}
-
-	template<typename VB, typename RT>
-	inline void rasterizer<VB, RT>::set_vertex_buffer(
-			std::shared_ptr<resource<VB>> in_vertex_buffer)
-	{
-		vertex_buffer = in_vertex_buffer;
-	}
-
-	template<typename VB, typename RT>
-	inline void rasterizer<VB, RT>::set_index_buffer(
-			std::shared_ptr<resource<unsigned int>> in_index_buffer)
-	{
-		index_buffer = in_index_buffer;
-	}
-
-	template<typename VB, typename RT>
-	inline void rasterizer<VB, RT>::draw(size_t num_vertexes, size_t vertex_offset)
-	{
-		// TODO Lab: 1.04 Implement `cg::world::camera` class
-		// TODO Lab: 1.05 Add `Rasterization` and `Pixel shader` stages to `draw` method of `cg::renderer::rasterizer`
-		// TODO Lab: 1.06 Add `Depth test` stage to `draw` method of `cg::renderer::rasterizer`
-	}
-
-	template<typename VB, typename RT>
-	inline int
-	rasterizer<VB, RT>::edge_function(int2 a, int2 b, int2 c)
-	{
-		// TODO Lab: 1.05 Implement `cg::renderer::rasterizer::edge_function` method
-		return 0;
-	}
-
-	template<typename VB, typename RT>
-	inline bool rasterizer<VB, RT>::depth_test(float z, size_t x, size_t y)
-	{
-		if (!depth_buffer)
-		{
-			return true;
-		}
-		return depth_buffer->item(x, y) > z;
-	}
-
+	extern std::shared_ptr<renderer> make_renderer(std::shared_ptr<cg::settings> settings);
 }// namespace cg::renderer
--- src/renderer/rasterizer/rasterizer_renderer.h
+++ src/renderer/rasterizer/rasterizer_renderer.h
@@ -1,7 +1,11 @@
+#pragma once
+
+#include "rasterizer.h"
 #include "renderer/rasterizer/rasterizer.h"
 #include "renderer/renderer.h"
 #include "resource.h"
-
+#include "world/camera.h"
+#include "world/model.h"
 
 namespace cg::renderer
 {
@@ -9,15 +13,27 @@ namespace cg::renderer
 	{
 	public:
 		virtual void init();
-		virtual void destroy();
-
-		virtual void update();
 		virtual void render();
+		virtual void destroy();
+		virtual void update();
 
 	protected:
 		std::shared_ptr<cg::resource<cg::unsigned_color>> render_target;
 		std::shared_ptr<cg::resource<float>> depth_buffer;
 
 		std::shared_ptr<cg::renderer::rasterizer<cg::vertex, cg::unsigned_color>> rasterizer;
+
+	private:
+		std::shared_ptr<cg::world::model> model;
+		std::shared_ptr<cg::world::camera> camera;
+
+		// Grayscale Conversion default parameters
+		bool enable_grayscale_conversion = false;
+		float grayscale_weight_r = 0.299f;
+		float grayscale_weight_g = 0.587f;
+		float grayscale_weight_b = 0.114f;
+
+		// Grayscale Conversion
+		cg::color convert_to_grayscale(const cg::color& pixel);
 	};
-}// namespace cg::renderer
+}
